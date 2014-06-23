1ddb2900a51209008cc6aaec1a58b9cfa6d64e14
RustyRadio <danielc@my.ccsu.edu>
2014-06-23 17:08:50 -0400

    Update

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -2,17 +2,47 @@
 
 import Cocoa
 
-func characterSplit(text: String) -> String[] {
-    var stringArray = String[]()
-    var counter = 0
-    for character in text {
-        stringArray[counter] = character
-        counter++
-    }
-    return stringArray
+let n = 10
+let names:UInt32 = 26
+var letterMap = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var xMap = Int(n)
+var yMap = Int(n)
+var namesArray = Array<Array<String>>()
+for column in 0..yMap {
+    namesArray.append(Array(count:xMap, repeatedValue:String()))
 }
 
-var testString = "###############"
-let testArray = testString.componentsSeparatedByString<#separator: String#>)
+func compareArray(array:Array<Array<String>>, firstName: String, lastName: String) ->( boolR: Bool) {
+    var arrayS = array, f = firstName, l = lastName, boolR = true
+    for var counter = 0; counter < arrayS.count; counter++ {
+        if arrayS[counter][0] == f && arrayS[counter][1] == l {
+            boolR = false
+        }
+    }
+    return (boolR)
+}
 
+func createName() ->  (aname: String) {
+    var aname: String = ""
+    var r = Int(arc4random_uniform(8) + 1)
+    for var i = 0; i < r; i++ {
+        var q = Int(arc4random_uniform(26) + 1)
+        aname += letterMap[q]
+    }
+    return aname
+}
 
+for (var i = 0; i < n; i++ ){
+    var firstName = createName()
+    var lastName = createName()
+    while !compareArray(namesArray, firstName, lastName).boolR {
+        var firstName = createName()
+        var lastName = createName()
+    }
+    var first = String(Int(arc4random_uniform(100) + 1)), second = String(Int(arc4random_uniform(100) + 1)), third = String(Int(arc4random_uniform(100) + 1)), fourth = String(Int(arc4random_uniform(100) + 1)), fifth = String(Int(arc4random_uniform(100) + 1))
+    var arrayAppend = [firstName, lastName, first, second, third, fourth, fifth]
+    namesArray[i] = arrayAppend
+    
+}
+
+namesArray
